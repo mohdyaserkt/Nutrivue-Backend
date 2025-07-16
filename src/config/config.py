fe92960852099262523edde718a2b123e9673693
yassar <yaserkt786@gmail.com>
2025-07-16 16:02:41 +0530

    fixed cors error

--- src/config/config.py
+++ src/config/config.py
@@ -1,7 +1,6 @@
 from pydantic_settings import BaseSettings
-from pydantic import AnyHttpUrl, validator
+from pydantic import validator
 from typing import List, Optional, Union
-import os
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "nutrivue"
@@ -9,29 +8,48 @@ class Settings(BaseSettings):
     VERSION: str = "1.0.0"
     
     API_V1_STR: str = "/api/v1"
-    ENV:str # or maybe Optional[str]
-    # Server configuration
+    ENV: Optional[str] = "dev"
     HOST: str = "0.0.0.0"
     PORT: int = 8000
-    RELOAD: bool = os.getenv("ENV", "dev") == "dev"
-    WORKERS: int = 1 if RELOAD else 4
     
     # CORS
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
-        "http://localhost:5173",  # Default frontend URL
-    ]
-    
+    BACKEND_CORS_ORIGINS: Union[str, List[str]] = []
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v):
+        if isinstance(v, str):
+            v = v.strip("[]").replace('"', '').replace("'", "")
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, list):
+            return v
+        raise ValueError(v)
+
+    @property
+    def RELOAD(self):
+        return self.ENV == "dev"
+
+    @property
+    def WORKERS(self):
+        return 1 if self.RELOAD else 4
+
     # Firebase
     FIREBASE_CREDENTIALS_PATH: str = "./serviceAccountKey.json"
-    DATABASE_URL_SYNC:str
-    DB_USER:str
-    DB_PASSWORD:str
-    DB_HOST:str
-    DB_PORT:str
-    DB_NAME:str
+    
+    # Database
+    DATABASE_URL_SYNC: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: str
+    DB_NAME: str
+    
+    # APIs
     GOOGLE_API_KEY: str
+    
+    # Redis
     REDIS_URL: str
     CACHE_EXPIRE_HOURS: int
+
     class Config:
         case_sensitive = True
         env_file = ".env"
